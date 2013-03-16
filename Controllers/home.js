71d4bbf5a05054529785fcea94745db2e09b84c1
Jacob <jladuval@gmail.com>
2013-03-16 05:58:53 -0400

    Views are now rendering with ejs

--- Controllers/home.js
+++ Controllers/home.js
@@ -2,18 +2,15 @@ var models = require('../DB/models');
 var transaction = require('../DB/transaction');
 
 exports.GetHome = function(req, res){
-  var body = 'Hello Worldasdfsadfsdf';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-  res.send('Hello Worldasdasdasd');
+ res.render('../Views/Home/index.ejs', {
+    layout:false
+});
   
-  transaction.Add(function(){
-      console.log('2');
-      var user = new models.User();
-      user.title = "TEST TITLE5";
-      user.save();
-  });
-  
-  transaction.Commit();
+//  transaction.Add(function(){
+//      var user = new models.User();
+//      user.title = "TEST TITLE5";
+//      user.save();
+//  });
+//  
+//  transaction.Commit();
 };
